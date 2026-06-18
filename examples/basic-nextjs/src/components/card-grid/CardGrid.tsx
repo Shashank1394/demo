07c5ee0e117594be81e60243960494a0a07df4d0
Shashank Jadhav <shashankjadhav13940@gmail.com>
2026-06-18 15:59:54 +0530

    Add RichText field support to CardGrid component and update props definition

--- examples/basic-nextjs/src/components/card-grid/CardGrid.tsx
+++ examples/basic-nextjs/src/components/card-grid/CardGrid.tsx
@@ -1,4 +1,4 @@
-import { Text } from "@sitecore-content-sdk/nextjs";
+import { RichText, Text } from "@sitecore-content-sdk/nextjs";
 import { CardGridProps } from "./card-grid.props";
 
 const CardGrid = (props: CardGridProps) => {
@@ -30,6 +30,14 @@ const CardGrid = (props: CardGridProps) => {
                   tag="p"
                   field={card.fields.Description}
                   className="text-gray-600"
+                />
+              )}
+
+              {card.fields?.RichText && (
+                <RichText
+                  tag="p"
+                  field={card.fields.RichText}
+                  className="text-gray-800"
                 />
               )}
             </article>
--- examples/basic-nextjs/src/components/card-grid/card-grid.props.ts
+++ examples/basic-nextjs/src/components/card-grid/card-grid.props.ts
@@ -15,5 +15,6 @@ export interface CardItem {
   fields: {
     Title?: Field<string>;
     Description?: Field<string>;
+    RichText?: Field<string>;
   };
 }
