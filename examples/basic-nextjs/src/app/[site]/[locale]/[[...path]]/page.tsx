55d1a95fc18e4649630c55907fe998d93d0cf8cb
Shashank Jadhav <shashank.jadhav@techxot.com>
2026-08-12 13:21:17 +0530

    refactor: remove debug logging from Page component

--- examples/basic-nextjs/src/app/[site]/[locale]/[[...path]]/page.tsx
+++ examples/basic-nextjs/src/app/[site]/[locale]/[[...path]]/page.tsx
@@ -25,13 +25,6 @@ type PageProps = {
 
 export default async function Page({ params }: PageProps) {
   const { site, locale, path } = await params;
-
-  console.log("========== SITECORE PAGE REQUEST ==========");
-  console.log("site:", site);
-  console.log("locale:", locale);
-  console.log("path:", path);
-  console.log("==========================================");
-
   const draft = await draftMode();
 
   setRequestLocale(`${site}_${locale}`);
