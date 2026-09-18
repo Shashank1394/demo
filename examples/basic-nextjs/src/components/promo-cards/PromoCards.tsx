e30a541e0141cd4aea3cf6279e41c2b030c22a13
Shashank Jadhav <shashankjadhav13940@gmail.com>
2026-09-18 12:47:30 +0530

    Refactor PromoCards components by removing specific variants and simplifying the Default export

--- examples/basic-nextjs/src/components/promo-cards/PromoCards.tsx
+++ examples/basic-nextjs/src/components/promo-cards/PromoCards.tsx
@@ -2,17 +2,5 @@ import { ComponentProps } from "../../lib/component-props";
 import { PromoCardsDefault } from "./PromoCardsDefault.dev";
 
 export const Default = (props: ComponentProps) => {
-  return <PromoCardsDefault {...props} variant="2-in-row" />;
-};
-
-export const TwoInRow = (props: ComponentProps) => {
-  return <PromoCardsDefault {...props} variant="2-in-row" />;
-};
-
-export const ThreeInRow = (props: ComponentProps) => {
-  return <PromoCardsDefault {...props} variant="3-in-row" />;
-};
-
-export const FourInRow = (props: ComponentProps) => {
-  return <PromoCardsDefault {...props} variant="4-in-row" />;
+  return <PromoCardsDefault {...props} />;
 };
--- examples/basic-nextjs/src/components/promo-cards/PromoCardsDefault.dev.tsx
+++ examples/basic-nextjs/src/components/promo-cards/PromoCardsDefault.dev.tsx
@@ -1,83 +1,15 @@
-import { Image, Link, RichText, Text } from "@sitecore-content-sdk/nextjs";
+import type { ComponentProps } from "../../lib/component-props";
 
-import type { PromoCardItem, PromoCardsProps } from "./promo-cards.props";
-
-type PromoCardContentProps = {
-  item: PromoCardItem;
-  columnClass: string;
-};
-
-const PromoCardContent = ({ item, columnClass }: PromoCardContentProps) => {
-  return (
-    <div className={columnClass}>
-      <div className="promo-card">
-        {item.Image && (
-          <div className="promo-card__image">
-            <Image field={item.Image} />
-          </div>
-        )}
-
-        <div className="promo-card__content">
-          {item.Title && (
-            <h3 className="promo-card__title">
-              <Text field={item.Title} />
-            </h3>
-          )}
-
-          {item.Description && (
-            <div className="promo-card__description">
-              <RichText field={item.Description} />
-            </div>
-          )}
-
-          {item.CTA && (
-            <div className="promo-card__cta">
-              <Link field={item.CTA} />
-            </div>
-          )}
-        </div>
-      </div>
-    </div>
-  );
-};
-
-export const PromoCardsDefault = (props: PromoCardsProps) => {
-  const datasource = props.fields?.data?.datasource;
-  console.log("PromoCardsDefault props:", props);
-  console.log("PromoCardsDefault datasource:", datasource);
-
-  const listTitle = datasource?.ListTitle;
-
-  const cards = datasource?.children?.results ?? [];
-
-  const columnClass = {
-    "2-in-row": "col-12 col-md-6",
-    "3-in-row": "col-12 col-md-6 col-lg-4",
-    "4-in-row": "col-12 col-md-6 col-lg-3",
-  }[props.variant ?? "3-in-row"];
+export const PromoCardsDefault = (props: ComponentProps) => {
+  console.log("PromoCards component loaded");
+  console.log("PromoCards props:", props);
 
   return (
     <div className="promo-cards">
       <div className="component-content">
         <div className="container">
-          {listTitle && (
-            <div className="row">
-              <div className="col-12">
-                <h2 className="promo-cards__title">
-                  <Text field={listTitle} />
-                </h2>
-              </div>
-            </div>
-          )}
-
           <div className="row">
-            {cards.map((item) => (
-              <PromoCardContent
-                key={item.id}
-                item={item}
-                columnClass={columnClass}
-              />
-            ))}
+            <p>PromoCards component is working.</p>
           </div>
         </div>
       </div>
